d849b561df57459ed35e4b11c062a253421060e7
Dusko Ojdanic <dusko.ojdanic@gmail.com>
2020-06-29 23:44:42 +0200

    helpers: create_and_drop

---
+++ src/helpers.rs
@@ -0,0 +1,61 @@
+use persy::{Config, PRes, Persy};
+use std::sync::{Condvar, Mutex};
+use tempfile::Builder;
+
+#[allow(dead_code)]
+pub struct CountDown {
+    lock: Mutex<u64>,
+    cond: Condvar,
+}
+
+#[allow(dead_code)]
+impl CountDown {
+    pub fn new(count: u64) -> CountDown {
+        CountDown {
+            lock: Mutex::new(count),
+            cond: Condvar::new(),
+        }
+    }
+
+    pub fn wait(&self) -> PRes<bool> {
+        let guard = self.lock.lock()?;
+        if *guard != 0 {
+            let _ = self.cond.wait(guard)?;
+        }
+        Ok(true)
+    }
+
+    pub fn count_down(&self) -> PRes<()> {
+        let mut count = self.lock.lock()?;
+        *count = (*count) - 1;
+        if *count == 0 {
+            self.cond.notify_all();
+        }
+        Ok(())
+    }
+}
+
+#[allow(dead_code)]
+pub fn create_and_drop<F>(name: &str, test: F)
+where
+    F: FnOnce(&Persy),
+{
+    create_and_drop_with_config(name, Config::new(), test);
+}
+
+#[allow(dead_code)]
+pub fn create_and_drop_with_config<F>(name: &str, config: Config, test: F)
+where
+    F: FnOnce(&Persy),
+{
+    let file = Builder::new()
+        .prefix(name)
+        .suffix(".persy")
+        .tempfile()
+        .expect("expect temp file creation");
+    Persy::create_from_file(file.reopen().expect("reopen")).expect(&format!("file '{:?}' do not exist", file));
+    {
+        let persy = Persy::open_from_file(file.reopen().expect("reopen"), config).unwrap();
+        test(&persy);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ extern crate test;
 #[allow(soft_unstable)]
 // #![cfg_attr(test, feature(test))]
 
+mod helpers;
+
 pub fn add_two(a: i32) -> i32 {
     a + 2
 }
@@ -11,10 +13,12 @@ pub fn add_two(a: i32) -> i32 {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use test::Bencher;
+    use persy::{Persy, PersyError, PersyId, Value, ValueMode};
     use std::path::Path;
     use std::str;
-    use persy::{Persy, PersyError, PersyId, Value, ValueMode};
+    use test::Bencher;
+    use crate::helpers::create_and_drop;
+    use std::time::Instant;
 
     #[test]
     fn it_works() {
@@ -27,17 +31,17 @@ mod tests {
     }
 
     // #[bench]
-    fn persy_save(b: &mut Bencher){
+    fn persy_save(b: &mut Bencher) {
         let persy = persy_setup();
         b.iter(|| save_one_read_one(&persy));
     }
     #[bench]
-    fn persy_load(b: &mut Bencher){
+    fn persy_load(b: &mut Bencher) {
         let persy = persy_setup();
         b.iter(|| persy_examine(&persy));
     }
 
-    fn persy_setup() -> Persy{
+    fn persy_setup() -> Persy {
         //https://gitlab.com/tglman/persy/-/blob/master/examples/indexes.rs
         let create_segment;
         if !Path::new("index.exp").exists() {
@@ -46,12 +50,13 @@ mod tests {
         } else {
             create_segment = false;
         }
-    
-        let persy : Persy = Persy::open("index.exp", persy::Config::new()).expect("open");
+
+        let persy: Persy = Persy::open("index.exp", persy::Config::new()).expect("open");
         if create_segment {
             let mut tx = persy.begin().expect("Transaction failed to start");
             tx.create_segment("data").expect("segment");
-            tx.create_index::<String, PersyId>("index", ValueMode::REPLACE).expect("index");
+            tx.create_index::<String, PersyId>("index", ValueMode::REPLACE)
+                .expect("index");
             let prepared = tx.prepare_commit().expect("prepare");
             prepared.commit().expect("commit");
         }
@@ -59,16 +64,19 @@ mod tests {
         persy
     }
 
-    fn save_one_read_one(persy: &Persy){
+    fn save_one_read_one(persy: &Persy) {
         let mut tx = persy.begin().expect("begin");
         let rec = "aaaa".as_bytes();
         let id = tx.insert_record("data", rec).expect("insert");
-    
-        tx.put::<String, PersyId>("index", "key".to_string(), id).expect("put");
+
+        tx.put::<String, PersyId>("index", "key".to_string(), id)
+            .expect("put");
         let prepared = tx.prepare_commit().expect("prep");
         prepared.commit().expect("c2");
 
-        let read_id = persy.get::<String, PersyId>("index", &"key".to_string()).expect("read id");
+        let read_id = persy
+            .get::<String, PersyId>("index", &"key".to_string())
+            .expect("read id");
         if let Some(is_there) = read_id {
             if let Value::SINGLE(id) = is_there {
                 let value = persy.read_record("data", &id).expect("read");
@@ -78,30 +86,63 @@ mod tests {
     }
 
     // #[test]
-    pub fn persy_examine(persy: &Persy){
+    pub fn persy_examine(persy: &Persy) {
         // let persy : Persy = Persy::open("index.exp", persy::Config::new()).expect("open");
 
-        let items = persy
-            .scan("data").expect("scan error");
-            let mut cnt = 0;
-            for (_id, content) in items {
-                cnt += 1;
-                let st = str::from_utf8(&content).unwrap();
-                println!("{}.", cnt);
-                println!("string : {}", st);
-                if false {
-                    println!("_id: {}", _id);
-                    println!("content: {:#?}", content);
-                }
+        let items = persy.scan("data").expect("scan error");
+        let mut cnt = 0;
+        for (_id, content) in items {
+            cnt += 1;
+            let st = str::from_utf8(&content).unwrap();
+            println!("{}.", cnt);
+            println!("string : {}", st);
+            if false {
+                println!("_id: {}", _id);
+                println!("content: {:#?}", content);
             }
-            // println!("num items: {}", items.len());
+        }
+        // println!("num items: {}", items.len());
     }
 
-}
+    #[test]
+    //https://gitlab.com/tglman/persy/-/blob/master/tests/record_operations.rs#L180
+    fn test_insert_100_same_tx() {
+        /*
+         use std::time::Instant;
+    let now = Instant::now();
 
+    {
+        my_function_to_measure();
+    }
+
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2}", elapsed);
+        */
+        let now = Instant::now();
+        create_and_drop("i100", |persy| {
+            let mut tx = persy.begin().unwrap();
+            tx.create_segment("test").unwrap();
+            let bytes = String::from("something").into_bytes();
+            for _ in [0; 100].iter() {
+                tx.insert_record("test", &bytes).unwrap();
+            }
+            let finalizer = tx.prepare_commit().unwrap();
+            finalizer.commit().unwrap();
+    
+            let mut count = 0;
+            for _ in persy.scan("test").unwrap() {
+                count += 1;
+            }
+            assert_eq!(count, 100);
+        });
+
+        let elapsed = now.elapsed();
+        println!("Elapsed: {:#?}", elapsed);
+    }
+    
+    
+}
 
 fn main() {
     println!("Hello, world!");
 }
-
-
